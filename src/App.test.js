3dcdf673b7f90361c761e6733f36a135b9366d72
jak307 <113345839+jak307@users.noreply.github.com>
2024-01-25 03:03:05 -0500

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,6 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
+test('renders login button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/login with spotify/i);
   expect(linkElement).toBeInTheDocument();
 });
+
